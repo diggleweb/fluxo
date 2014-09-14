45801a478c66c9c58c4a8849f2cc5ff9135dd6e5
Michael Granados <michaelgranados@gmail.com>
2014-09-14 18:20:18 -0300

    Making all pages for categories
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,7 +23,6 @@ class CategoriesController < ApplicationController
 
   # POST /categories
   def create
-    puts params[:category]
     @category = Category.new category_params
 
     respond_to do |format|
@@ -34,6 +33,50 @@ class CategoriesController < ApplicationController
         format.html  { render :action => "new" }
         format.json  { render :json => @category.errors, :status => :unprocessable_entity }
       end
+    end
+  end
+
+  # GET /categories/:id
+  def show
+    @category = Category.find params[:id]
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :jsonn => @category }
+    end
+  end
+
+  # GET /categories/:id/edit
+  def edit
+    @category = Category.find params[:id]
+    respond_to do |format|
+      format.html # edit.html.erb
+      format.json { render :jsonn => @category }
+    end
+  end
+
+  # PATH/PUT /categories/:id
+  def update
+    @category = Category.find params[:id]
+    
+    respond_to do |format|
+      if @category.update category_params
+        format.html  { redirect_to(@category, :notice => 'category was successfully updated.') }
+        format.json  { render :json => @category, :status => :created, :location => @category }
+      else
+        format.html  { render :action => "edit" }
+        format.json  { render :json => @category.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /categories/:id
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+
+    respond_to do |format|
+      format.html  { redirect_to categories_path, notice: 'categoria apagada!' }
+      format.json  { render :nothing }
     end
   end
 
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,6 @@
 module CategoriesHelper
+	def category_bullet color
+		color = color.color if Category == color.class
+		"<span class=\"category-bullet\" style=\"background: #{color}\"></span>".html_safe
+	end
 end
