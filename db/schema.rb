d598d7c0c98acbd4ab02ee6e602ab02da43bb396
Michael Granados <michaelgranados@gmail.com>
2014-09-20 17:11:55 -0300

    Adding colun balance to account
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140914212103) do
+ActiveRecord::Schema.define(version: 20140920200646) do
 
   create_table "accounts", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "balance",     precision: 8, scale: 2
   end
 
   create_table "categories", force: true do |t|
