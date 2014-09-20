f56a2c9649968fc9a52642dcb555636deb70e8b3
Michael Granados <michaelgranados@gmail.com>
2014-09-20 19:16:32 -0300

    Moving options helper to application_helper
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def options model
+		model.map { |item| [item.name, item.id] }
+	end
 end
--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,5 +1,2 @@
 module TransactionsHelper
-	def options model
-		model.map { |item| [item.name, item.id] }
-	end
 end
